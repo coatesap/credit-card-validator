5658c4893ddaa759fd4264fadcc3c841705ded60
Andy Coates <andy@websitevision.co.uk>
2014-12-02 09:26:09 +0000

    Updated code to Symfony 2 standard

--- README.md
+++ README.md
@@ -4,29 +4,28 @@ Credit Card Class
 Simple PHP class for preparing and validating credit/debit card details. Typically this takes place before they are passed to a payment gateway for processing.
 
 
-Example Usage
--------------
+## Example Usage
 
 ```PHP
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
-$card_details = array(
-	'number' =>'4929000000006', 
-	'expiry_year' => 2013,
-	'expiry_month' => 5,
-	'code' => '123'
+$cardDetails = array(
+    'number' =>'4929000000006',
+    'expiryYear' => 2013,
+    'expiryMonth' => 5,
+    'code' => '123'
 );
 
 $card = new Coatesap\Payment\Card();
-$card->populate($card_details);
+$card->populate($cardDetails);
 
-if (!$card->is_valid($message)) {
+if (!$card->isValid($message)) {
     // show error message
     echo 'There is a problem with your card details: ' . $message;
 } else {
     // get the prepped, validated card data as an array
-    $card_data = $card->to_array();
-    // send $card_data to payment gateway
+    $cardData = $card->toArray();
+    // send $cardData to payment gateway
 }
 ```
 
@@ -38,20 +37,20 @@ to your `composer.json` file:
 ```json
 {
     "require": {
-        "coatesap/credit-card-validator": "~1.0"
+        "coatesap/credit-card-validator": "~2.0"
     }
 }
 ```
 
-Data preparation
-----------------
+## Data preparation
+
 This class does some simple preparation of your card data. This includes removing non-numeric characters, including spaces, from:
 - The card number
 - The CVC/CV2 value
 - The card expiry date
 
-Validation Checks
------------------
+## Validation Checks
+
 This class also checks:
 - That the card hasn't expired
 - A valid CVC/CV2 security code has been supplied
--- src/Coatesap/Payment/Card.php
+++ src/Coatesap/Payment/Card.php
@@ -1,124 +1,162 @@
-<?php 
+<?php
+
 namespace Coatesap\Payment;
 
-class Card {
-	
-	public $number;
-	public $code;
-	public $expiry_month = 0;
-	public $expiry_year = 0;
-	
-	function __construct()
-	{	
-	}
-	
-	public function has_expired()
-	{
-		if (version_compare(phpversion(), '5.2.0', '>='))
-		{
-			$date_string = $this->expiry_year . '-' . ($this->expiry_month+1) . '-01 00:00:00';
-			$expiry_date = \DateTime::createFromFormat('Y-m-d H:i:s', $date_string);
-			$today = new \DateTime();
-			return ($today >= $expiry_date) ? true : false ;	
-		} 
-		else
-		{
-			$card = mktime(0, 0, 0, $this->expiry_month, '01', $this->expiry_year);
-			$today = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
-			$cont = $card-$today;
-			return ($cont < 0) ? true : false ;
-		}
-		
-	}
-	
-	public function populate($data)
-	{
-		if (isset($data['number'])) $this->number = preg_replace('/[^0-9]/', '', $data['number']);
-		if (isset($data['code'])) $this->code = preg_replace('/[^0-9]/', '', $data['code']);
-		if (isset($data['expiry_month'])) {
-			$expiry_month = preg_replace('/[^0-9]/', '', $data['expiry_month']);
-			$this->expiry_month = (int) $expiry_month;
-		}
-		if (isset($data['expiry_year'])) {
-			$expiry_year = preg_replace('/[^0-9]/', '', $data['expiry_year']);
-			$this->expiry_year = (int) $expiry_year;
-		}
-	}
-	
-	public function is_valid(&$message='')
-	{
-		if (strlen($this->code) < 3) {
-			$message = 'Card security code should be at least 3 characters';
-			return false;	
-		}
-		if (strlen($this->code) > 4) {
-			$message = 'Card security code should be no more than 4 characters';
-			return false;	
-		}
-		if ($this->expiry_month == 0) {
-			$message = 'Expiry month is required';
-			return false;	
-		}
-		if ($this->expiry_month > 12) {
-			$message = 'Expiry month is not valid';
-			return false;	
-		}
-		if ($this->expiry_year == 0) {
-			$message = 'Expiry year is required';
-			return false;	
-		}
-		if (!$this->luhn_check($this->number)) {
-			$message = 'Card number is not valid';
-			return false;
-		}
-		if ($this->has_expired()) {
-			$message = 'Card has expired';
-			return false;	
-		}
-		
-		return true;
-	}
-	
-	private function luhn_check($number) 
-	{		
-		// Strip any non-digits (useful for credit card numbers with spaces and hyphens)
-		$number = preg_replace('/\D/', '', $number);
-		
-		// Set the string length and parity
-		$number_length = strlen($number);
-		$parity = $number_length % 2;
-		
-		// Loop through each digit and do the maths
-		$total = 0;
-		for ($i = 0; $i < $number_length; $i++) {
-			$digit = $number[$i];
-			// Multiply alternate digits by two
-			if ($i % 2 == $parity) {
-				$digit *= 2;
-				// If the sum is two digits, add them together (in effect)
-				if ($digit > 9) {
-					$digit -= 9;
-				}
-			}
-			// Total up the digits
-			$total += $digit;
-		}
-		
-		// If the total mod 10 equals 0, the number is valid
-		return ($total % 10 == 0) ? TRUE : FALSE;
-	
-	}
-	
-	// return an array representation of this card
-	public function to_array()
-	{
-		$arr = array();
-		$reflection = new \ReflectionObject($this);
-		$properties = $reflection->getProperties(\ReflectionProperty::IS_PUBLIC);
-		foreach ($properties as $obj) {
-			$arr[$obj->name] = $this->{$obj->name};	
-		}
-		return $arr;
-	}
-		
+class Card
+{
+
+    public $number;
+    public $code;
+    public $expiryMonth = 0;
+    public $expiryYear = 0;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Find out if the card has expired.
+     *
+     * @return bool
+     */
+    public function hasExpired()
+    {
+        if (version_compare(phpversion(), '5.2.0', '>=')) {
+            $dateString = $this->expiryYear . '-' . ($this->expiryMonth + 1) . '-01 00:00:00';
+            $expiry_date = \DateTime::createFromFormat('Y-m-d H:i:s', $dateString);
+            $today = new \DateTime();
+
+            return ($today >= $expiry_date) ? true : false;
+        } else {
+            $card = mktime(0, 0, 0, $this->expiryMonth, '01', $this->expiryYear);
+            $today = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+            $cont = $card - $today;
+
+            return ($cont < 0) ? true : false;
+        }
+
+    }
+
+    /**
+     * Bulk fill the card with data.
+     *
+     * @param array $data
+     */
+    public function populate($data)
+    {
+        if (isset($data['number'])) {
+            $this->number = preg_replace('/[^0-9]/', '', $data['number']);
+        }
+        if (isset($data['code'])) {
+            $this->code = preg_replace('/[^0-9]/', '', $data['code']);
+        }
+        if (isset($data['expiryMonth'])) {
+            $expiryMonth = preg_replace('/[^0-9]/', '', $data['expiryMonth']);
+            $this->expiryMonth = (int)$expiryMonth;
+        }
+        if (isset($data['expiryYear'])) {
+            $expiryYear = preg_replace('/[^0-9]/', '', $data['expiryYear']);
+            $this->expiryYear = (int)$expiryYear;
+        }
+    }
+
+    /**
+     * Validate card details.
+     *
+     * @param string $message Error message returned by reference
+     * @return bool
+     */
+    public function isValid(&$message = '')
+    {
+        if (strlen($this->code) < 3) {
+            $message = 'Card security code should be at least 3 characters';
+
+            return false;
+        }
+        if (strlen($this->code) > 4) {
+            $message = 'Card security code should be no more than 4 characters';
+
+            return false;
+        }
+        if ($this->expiryMonth == 0) {
+            $message = 'Expiry month is required';
+
+            return false;
+        }
+        if ($this->expiryMonth > 12) {
+            $message = 'Expiry month is not valid';
+
+            return false;
+        }
+        if ($this->expiryYear == 0) {
+            $message = 'Expiry year is required';
+
+            return false;
+        }
+        if (!$this->luhnCheck($this->number)) {
+            $message = 'Card number is not valid';
+
+            return false;
+        }
+        if ($this->hasExpired()) {
+            $message = 'Card has expired';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether a card number passes the Luhn check.
+     *
+     * @param string|int $number Card number
+     * @return bool
+     */
+    private function luhnCheck($number)
+    {
+        // Strip any non-digits (useful for credit card numbers with spaces and hyphens)
+        $number = preg_replace('/\D/', '', $number);
+
+        // Set the string length and parity
+        $numberLength = strlen($number);
+        $parity = $numberLength % 2;
+
+        // Loop through each digit and do the maths
+        $total = 0;
+        for ($i = 0; $i < $numberLength; $i++) {
+            $digit = $number[$i];
+            // Multiply alternate digits by two
+            if ($i % 2 == $parity) {
+                $digit *= 2;
+                // If the sum is two digits, add them together (in effect)
+                if ($digit > 9) {
+                    $digit -= 9;
+                }
+            }
+            // Total up the digits
+            $total += $digit;
+        }
+
+        // If the total mod 10 equals 0, the number is valid
+        return ($total % 10 == 0) ? true : false;
+
+    }
+
+    /**
+     * Creates an array representation of the card.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $arr = array();
+        $reflection = new \ReflectionObject($this);
+        $properties = $reflection->getProperties(\ReflectionProperty::IS_PUBLIC);
+        foreach ($properties as $obj) {
+            $arr[$obj->name] = $this->{$obj->name};
+        }
+
+        return $arr;
+    }
 }
